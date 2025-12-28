9a7b77529bc2dc034dcd0bcd5efc16890b59fba5
lelouchZhang <zhch8128@gmail.com>
2025-12-28 23:55:14 +0800

    create: Header & 404 page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
-  return (
-    <div className='text-3xl text-indigo-500'>page</div>
-  )
-}
+  return <div className="text-3xl text-indigo-500"></div>;
+};
 
-export default page
+export default page;
